6f7440ff53e7d3e24e8face023864ca5e6a19aad
muhtamim <muhtamim.lu@gmail.com>
2019-11-29 16:45:35 +0700

    Initial commit

---
+++ app/src/main/java/com/example/m/doctorchacha/FirstAidCard.java
@@ -0,0 +1,23 @@
+package com.example.m.doctorchacha;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class FirstAidCard {
+
+    int id;
+    String title,content;
+    String imageId;
+
+    FirstAidCard(int id,String title,String content,String imageId)
+    {
+        this.title = title;
+        this.id=id;
+        this.content = content;
+        this.imageId = imageId;
+    }
+
+
+}
